49859c22752492bd7d980eedaef9e2d801111ded
AdmiralKeso <cas.hagenaar@gmail.com>
2025-03-02 14:38:52 +0100

    Add: First 3 button function to behave on their own
    When one button is clicked. Remove the class from the other 2 buttons. Therefore you can only click 1 at a time.

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,29 @@ button1.addEventListener('click', () => handleButton1To3(button1, prices[0]));
 button2.addEventListener('click', () => handleButton1To3(button2, prices[1]));
 button3.addEventListener('click', () => handleButton1To3(button3, prices[2]));
 
+// Function for button 1-3
+function handleButton1To3(button, price) {
+    // If the clicked button is already pressed, deselect it
+    if (button.classList.contains('pressed')) {
+        button.classList.remove('pressed');
+        price.classList.remove('update');
+    } else {
+        // Reset all three buttons to unselected state
+        button1.classList.remove('pressed');
+        button2.classList.remove('pressed');
+        button3.classList.remove('pressed');
+        
+        // Remove the 'update' class from all prices
+        prices[0].classList.remove('update');
+        prices[1].classList.remove('update');
+        prices[2].classList.remove('update');
+        
+        button.classList.add('pressed');
+        price.classList.add('update');
+    }
+}
+
+// Event listeners for all other buttons (4–9)
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
         button.classList.toggle('pressed');
