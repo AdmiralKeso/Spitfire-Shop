2f2336ae809165d0f257262c4c7c6b1041b964ea
AdmiralKeso <cas.hagenaar@gmail.com>
2025-03-02 14:35:35 +0100

    Add: Declare the first 3 buttons as variables

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 const buttons = document.querySelectorAll('.toggle-btn');
 const prices = document.querySelectorAll('.toggle-price');
+const button1 = document.getElementById("color-1");
+const button2 = document.getElementById("color-2");
+const button3 = document.getElementById("color-3");
 
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
