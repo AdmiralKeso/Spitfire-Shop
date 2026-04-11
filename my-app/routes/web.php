6d66cd1004789582a248d438ea15c239df9c4e4e
AdmiralKeso <cas.hagenaar@gmail.com>
2026-04-11 19:21:25 +0200

    Update routes to match functions with controller

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'index'])->name('home');
+Route::get('/forum', [MainController::class, 'forum'])->name('forum');
+Route::get('/forum/create', [MainController::class, 'forumCreate'])->name('create');
+Route::get('/account', [MainController::class, 'account'])->name('account');
